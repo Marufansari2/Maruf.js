dc40a0f332ed36fb6536521f40b97d661a82f4e2
Marufansari2 <marufansarim1@gmail.com>
2026-03-27 00:41:42 +0000

    conversion of datatypes

--- 1_basics/conversion.js
+++ 1_basics/conversion.js
@@ -1,4 +1,4 @@
-let score=false
+/*let score=false
 console.log(typeof score);
 console.log(typeof (score));
 
@@ -17,4 +17,32 @@ let someNumber = 55
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+// operations
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+//console.log(2**3); // it become cube of 2
+
+// console.log(2%3);
+
+/*console.log("1" +2);
+console.log(1+ "2");
+
+console.log("1" +2+2);
+
+console.log(1+2+"2");
+
+console.log(3 + 4 *5 % 3); */ //why we need to weite this type of code.
+
+console.log(+"");
+console.log(+true);
+
+
+let gameCounter=100
+gameCounter++;//
+console.log(gameCounter);
+
 
