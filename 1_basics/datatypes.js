1e441fe7ee13f5d5ac0e50889b4f80700fd9146f
Marufansari2 <marufansarim1@gmail.com>
2026-03-27 00:07:38 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat as new version
+
+//alert("Maruf") // we are using node.js not browser,it is run in console
+
+console.log(3+3) // we can also use semi colon there is not any affect on code
+// we can also use like
+console.log(3
+    +3
+) //it can be also run but not good for good code
+
+let name ="maruf"
+let age =20
+let isLoggedIn=false
+let state=null // object
+
+
+console.log(typeof null);
---
+++ 1_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12342
+let accountEmail="maruf@gmail.com"
+var accountPassword="1222"
+accountCity="Nagpur"
+let accountState;
+//accountId=5 // not alllowed because its const
+
+accountEmail="ansari@gm.com"
+accountPassword="3121"
+accountCity="UP"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
